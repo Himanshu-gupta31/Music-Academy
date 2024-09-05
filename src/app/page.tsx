08cb8b59418b9b4df6e056ed00aacf942841fa1f
Himanshu-gupta31 <himanshu.gupta0031@gmail.com>
2024-09-06 00:21:10 +0530

    added testi and why choose us section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import TestimonialsCard from "@/components/TestimonialsCard";
+import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center">Music Academy</h1>
+      {/* <h1 className="text-2xl text-center">Music Academy</h1> */}
       <HeroSection/>
+      <hr className="border-zinc-700"></hr>
+      <FeaturedCourses/>
+      <WhyChooseUs/>
+       
     </main>
   );
 }
